381a733c50653e1c4d9b1e72f6a854c44f3ba51a
super_dev <b.dev0812@gmail.com>
2024-02-08 09:01:16 -0500

    updated test script

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -1,16 +1,33 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaTwitter } from "../target/types/solana_twitter";
-
+import * as assert from "assert";
 describe("solana-twitter", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("can send a new tweet", async () => {    
+    const tweet = anchor.web3.Keypair.generate();
+    const wallet = (program.provider as anchor.AnchorProvider).wallet;
+    await program.methods.sendTweet('veganism', 'Hummus, am I right?').accounts({
+      tweet: tweet.publicKey,
+        author: wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId
+    })
+    .signers([tweet])
+    .rpc();
+    
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+  	console.log("tweet Account", tweetAccount);    
   });
+
+  it('can fetch all tweets', async () => {
+    const tweetAccounts = await program.account.tweet.all();
+    console.log("tweets", tweetAccounts);
+    assert.equal(tweetAccounts.length, 1);
+});
+
+
 });
